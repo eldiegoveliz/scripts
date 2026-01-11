f172d74e479dd1519ce53a5aea89d794d560493c
Diego Véliz <diego@diegoveliz.xyz>
2026-01-10 23:28:13 -0300

    organizacion de grafstoncks

--- grafstoncks/enabler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#activador
-source /home/diego/.venvs/myenv/bin/activate
-
-#ejecutar python
-python /home/diego/scripts/grafstoncks/ipsa.py
-python /home/diego/scripts/grafstoncks/sp500.py
